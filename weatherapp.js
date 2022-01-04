f11a489a6d8c43263b60fcdd947cb20137208e77
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2022-01-04 09:58:35 -0500

    updated comments

--- weatherapp.js
+++ weatherapp.js
@@ -33,36 +33,36 @@ cityButton.addEventListener("click", () => {
       // display city names
       displayCountryName.innerText = `Country name: ${data.sys.country}`;
      // display country name
-      displayFeelsLike.innerText = ` Feels like temperature ${Math.round(data.main.feels_like)} degree(s) Fahrenheit`;
+      displayFeelsLike.innerText = ` Feels like temperature ${Math.round(data.main.feels_like)} degree(s) Fahrenheit `;
       // display current feels like temperature in Fahrenheit
-      displayTemperature.innerText = `Current Temperature ${Math.round(data.main.temp)} degree(s) Fahrenheit`;
+      displayTemperature.innerText = `Current Temperature ${Math.round(data.main.temp)} degree(s) Fahrenheit `;
      // display current tempearture in Fahrenheit
-      displaymin.innerText = `Min temperature for today: ${Math.round(data.main.temp_min)} degree(s) Fahrenheit`;
+      displaymin.innerText = `Min temperature for today: ${Math.round(data.main.temp_min)} degree(s) Fahrenheit `;
       // display current minimum temperature in Fahrenheit
-      displaymax.innerText = `Max temperature for today: ${Math.round(data.main.temp_max)} degree(s) Fahrenheit`;
+      displaymax.innerText = `Max temperature for today: ${Math.round(data.main.temp_max)} degree(s) Fahrenheit `;
       // displays current maximum temperature in Fahrenheit
-      displayFeelsLikeCelsius.innerText = ` Feels like temperature ${Math.round((((data.main.feels_like)-32)*5)/9)} degree(s) Celsius`;
+      displayFeelsLikeCelsius.innerText = ` Feels like temperature ${Math.round((((data.main.feels_like)-32)*5)/9)} degree(s) Celsius `;
       // displays feels like temperature in Celsius
-      displayTemperatureCelsius.innerText = `Current Temperature ${Math.round((((data.main.temp)-32)*5)/9)} degree(s) Celsius`;
+      displayTemperatureCelsius.innerText = `Current Temperature ${Math.round((((data.main.temp)-32)*5)/9)} degree(s) Celsius `;
      // displays temperature in Celsius
-      displayminCelsius.innerText = `Min temperature for today: ${Math.round((((data.main.temp_min)-32)*5)/9)} degree(s) Celsius`;
+      displayminCelsius.innerText = `Min temperature for today: ${Math.round((((data.main.temp_min)-32)*5)/9)} degree(s) Celsius `;
       // displays minimum temperature in Celsius
-      displaymaxCelsius.innerText = `Max temperature for today: ${Math.round((((data.main.temp_max)-32)*5)/9)} degree(s) Celsius`;
+      displaymaxCelsius.innerText = `Max temperature for today: ${Math.round((((data.main.temp_max)-32)*5)/9)} degree(s) Celsius `;
       // displays maximum temperature in Celsius
-      displayHumidity.innerText = `Humidity: ${data.main.humidity}%`;
+      displayHumidity.innerText = `Humidity: ${data.main.humidity}% `;
       // displays humidity percentage
-      displayWindSpeed.innerText = `Windspeed: ${Math.round(data.wind.speed)} mile(s)/hour`;
+      displayWindSpeed.innerText = `Windspeed: ${Math.round(data.wind.speed)} mile(s)/hour `;
       // displays windspeed in miles per hour
-      const imageURL = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      const imageURL = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png `;
       // end point to display a weather icon from open weather map, the icon is dynamically updated 
       // by the icon provided from our api.
       displayIcon.innerHTML = `<img src=${imageURL}>`;
       // icon is an image so <img src = {}> is necessary
-      displayweatherdescription.innerText = `Current weather description: ${data.weather[0].description}`;
+      displayweatherdescription.innerText = `Current weather description: ${data.weather[0].description} `;
       // displays weather description (rain, sleet, snow)
     })
     .catch((error) => { 
-      displayError.innerText = `Error please input a city name`
+      displayError.innerText = `Error please input a city name `
       console.log(error)
     // In case there is an error
     })  
