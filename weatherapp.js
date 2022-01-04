67b5888de4f2d73736cbdbeb983991cb5a3ac3b6
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2022-01-04 09:30:59 -0500

    Updated comments

--- weatherapp.js
+++ weatherapp.js
@@ -21,31 +21,50 @@ const APIkey =  "a4a45650e5f60155d63b1c8b9d1efb59#";
 // authentication to access website, this will remain constant
 
 cityButton.addEventListener("click", () => {
-// callback function for fetch
-  let userInput=  userInputCity.value;
+// call back function this code won't run until citybutton is clicked
+  let userInput =  userInputCity.value;
+  // user decides what city they want and this will dynamically update our parameters
   const url =  `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=${unit}&appid=${APIkey}`;
-// endpoint with parameter to get city when user searches it
+// endpoint with parameters to get city and imperial units when user searches it
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
       displayCityName.innerText = ` City name: ${data.name}`;
+      // display city names
       displayCountryName.innerText = `Country name: ${data.sys.country}`;
+     // display country name
       displayFeelsLike.innerText = ` Feels like temperature ${Math.round(data.main.feels_like)} degrees Fahrenheit`;
+      // display current feels like temperature in Fahrenheit
       displayTemperature.innerText = `Current Temperature ${Math.round(data.main.temp)} degrees Fahrenheit`;
+     // display current tempearture in Fahrenheit
       displaymin.innerText = `Min temperature for today: ${Math.round(data.main.temp_min)} degrees Fahrenheit`;
+      // display current minimum temperature in Fahrenheit
       displaymax.innerText = `Max temperature for today: ${Math.round(data.main.temp_max)} degrees Fahrenheit`;
+      // displays current maximum temperature in Fahrenheit
       displayFeelsLikeCelsius.innerText = ` Feels like temperature ${Math.round((((data.main.feels_like)-32)*5)/9)} degrees Celsius`;
+      // displays feels like temperature in Celsius
       displayTemperatureCelsius.innerText = `Current Temperature ${Math.round((((data.main.temp)-32)*5)/9)} degrees Celsius`;
+     // displays temperature in Celsius
       displayminCelsius.innerText = `Min temperature for today: ${Math.round((((data.main.temp_min)-32)*5)/9)} degrees Celsius`;
+      // displays minimum temperature in Celsius
       displaymaxCelsius.innerText = `Max temperature for today: ${Math.round((((data.main.temp_max)-32)*5)/9)} degrees Celsius`;
+      // displays maximum temperature in Celsius
       displayHumidity.innerText = `Humidity: ${data.main.humidity}%`;
+      // displays humidity percentage
       displayWindSpeed.innerText = `Windspeed: ${Math.round(data.wind.speed)} mile(s)/hour`;
+      // displays windspeed in miles per hour
       const imageURL = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      // end point to display a weather icon from open weather map, the icon is dynamically updated 
+      // by the icon provided from our api.
       displayIcon.innerHTML = `<img src=${imageURL}>`;
+      // icon is an image so <img src = {}> is necessary
       displayweatherdescription.innerText = `Current weather description: ${data.weather[0].description}`;
+      // displays weather description (rain, sleet, snow)
     })
     .catch((error) => { 
       displayError.innerText = `Error please input a city name`
+      console.log(error)
+    // In case there is an error
     })  
   })
   
--- weatherappzip.js
+++ weatherappzip.js
@@ -28,23 +28,40 @@ const APIkey = "a4a45650e5f60155d63b1c8b9d1efb59#";
       .then((response) => response.json())
       .then((data) => {
         displayCityName.innerText =  ` City name: ${data.name}`;
+        // display city names
         displayCountryName.innerText =  `Country name: ${data.sys.country}`;
+        // display country name
         displayFeelsLike.innerText =  ` Feels like temperature ${Math.round(data.main.feels_like)} degrees Fahrenheit`;
+        // display current feels like temperature in Fahrenheit
         displayTemperature.innerText =  `Current Temperature ${Math.round(data.main.temp)} degrees Fahrenheit`;
+        // display current tempearture in Fahrenheit
         displaymin.innerText =  `Min temperature for today: ${Math.round(data.main.temp_min)} degrees Fahrenheit`;
+        // display current minimum temperature in Fahrenheit
         displaymax.innerText =  `Max temperature for today: ${Math.round(data.main.temp_max)} degrees Fahrenheit`;
+        // displays current maximum temperature in Fahrenheit
         displayFeelsLikeCelsius.innerText =  ` Feels like temperature ${Math.round((((data.main.feels_like)-32)*5)/9)} degrees Celsius`;
+        // displays feels like temperature in Celsius
         displayTemperatureCelsius.innerText =  `Current Temperature ${Math.round((((data.main.temp)-32)*5)/9)} degrees Celsius`;
+        // displays temperature in Celsius
         displayminCelsius.innerText =  `Min temperature for today: ${Math.round((((data.main.temp_min)-32)*5)/9)} degrees Celsius`;
+        // displays minimum temperature in Celsius
         displaymaxCelsius.innerText =  `Max temperature for today: ${Math.round((((data.main.temp_max)-32)*5)/9)} degrees Celsius`;
+        // displays maximum temperature in Celsius
         displayHumidity.innerText =  `Humidity: ${data.main.humidity}%`;
+         // displays humidity percentage
         displayWindSpeed.innerText =  `Windspeed: ${Math.round(data.wind.speed)} mile(s)/hour`;
+         // displays windspeed in miles per hour
         const imageURL =  `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        // end point to display a weather icon from open weather map, the icon is dynamically updated 
+      // by the icon provided from our api.
         displayIcon.innerHTML =  `<img src= ${imageURL}>`;
-        displayweatherdescription.innerText =  `Current weather description: ${data.weather[0].description}`;
+         // icon is an image so <img src = {}> is necessary
+        displayweatherdescription.innerText =  `Current weather description: ${data.weather[0].description}`; 
       })
+       // displays weather description (rain, sleet, snow)
       .catch((error) => { 
         displayError.innerText = `Error, input a US zip code`
+            // In case there is an error
       })  
     })
 
